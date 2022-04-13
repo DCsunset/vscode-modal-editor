65073f760297e346d8e512f3c21101df2d5e47e5
DCsunset <DCsunset@users.noreply.github.com>
2022-04-13 17:28:25 -0400

    feat: add preliminary helix preset

---
+++ presets/helix.js
@@ -0,0 +1,99 @@
+/**
+ * Keybindings for helix editor
+ */
+
+// actions shared by normal and select mode
+const actions = {
+  u: "undo",
+  U: "redo",
+	d: "deleteRight",
+	c: [
+		"deleteRight",
+		"modalEditor.setInsertMode"
+	],
+  x: "expandLineSelection",
+	"<": "editor.action.outdentLines",
+	">": "editor.action.indentLines",
+	y: "editor.action.clipboardCopyAction",
+	p: [
+		"cursorRight",
+		"editor.action.clipboardPasteAction"
+	],
+	P: "editor.action.clipboardPasteAction"
+};
+
+module.exports = {
+	normal: {
+    // cursor movement
+		h: "cursorLeft",
+		j: "cursorDown",
+		k: "cursorUp",
+		l: "cursorRight",
+		w: [
+			"cancelSelection",
+			"cursorWordStartRightSelect",
+		],
+	  b: [
+			"cancelSelection",
+			"cursorWordStartLeftSelect"
+		],
+		
+		// actions
+		...actions,
+    
+    // goto mode
+    g: {
+      h: "cursorHome",
+      l: "cursorEnd",
+			g: "cursorTop",
+			e: "cursorBottom",
+    },
+
+		// into insert mode
+		i: "modalEditor.setInsertMode",
+		I: [
+      "cursorHome",
+      "modalEditor.setInsertMode"
+    ],
+		a: [
+			"cursorRight",
+      "modalEditor.setInsertMode"
+		],
+		A: [
+      "cursorEnd",
+      "modalEditor.setInsertMode"
+    ],
+		o: [
+      "editor.action.insertLineAfter",
+      "modalEditor.setInsertMode"
+    ],
+		O: [
+      "editor.action.insertLineBefore",
+      "modalEditor.setInsertMode"
+    ],
+    
+		// set to select mode
+		v: "modalEditor.setSelectMode"
+  },
+	select: {
+    // cursor movement
+		h: "cursorLeftSelect",
+		j: "cursorDownSelect",
+		k: "cursorUpSelect",
+		l: "cursorRightSelect",
+		w: "cursorWordStartRightSelect",
+	  b: "cursorWordStartLeftSelect",
+    
+    // goto mode
+    g: {
+      h: "cursorHomeSelect",
+      l: "cursorEndSelect",
+			g: "cursorTopSelect",
+			e: "cursorBottomSelect",
+    },
+		
+		// actions
+		...actions
+	}
+};
+
