d3983abff2b86993d377a0934384017efc18a723
DCsunset <DCsunset@users.noreply.github.com>
2022-05-04 22:08:42 -0400

    feat: update helix preset

--- presets/helix.js
+++ presets/helix.js
@@ -26,7 +26,20 @@ module.exports = {
 		C: repeatable("editor.action.insertCursorBelow"),
 		",": "removeSecondaryCursors",
 
-		x: repeatable("expandLineSelection"),
+		J: repeatable("editor.action.joinLines"),
+		x: repeatable([
+			{
+				command: "cursorRightSelect",
+				// move right when this line is alreay selected (because of inclusive range)
+				when: "_ctx.selection.contains(_ctx.lineAt(_ctx.pos.line).range)"
+			},
+			"expandLineSelection",
+			{
+				// move left when it expands to a new line (because of inclusive range)
+				command: "cursorLeftSelect",
+				when: "!_ctx.lastSelection.contains(_ctx.lineAt(_ctx.lastPos.line).range)"
+			}
+		]),
 		"<": repeatable("editor.action.outdentLines"),
 		">": repeatable("editor.action.indentLines"),
 		y: "modalEditor.yank",
@@ -101,13 +114,33 @@ module.exports = {
 		},
 
 		// cursor movement
-		h: repeatable("cursorLeft"),
-		j: repeatable("cursorDown"),
-		k: repeatable("cursorUp"),
-		l: repeatable("cursorRight"),
+		h: repeatable([
+			// Cancel selection first or it will move before the selection
+			"cancelSelection",
+			"cursorLeft"
+		]),
+		j: repeatable([
+			"cancelSelection",
+			"cursorDown"
+		]),
+		k: repeatable([
+			"cancelSelection",
+			"cursorUp"
+		]),
+		l: repeatable([
+			"cancelSelection",
+			"cursorRight"
+		]),
 		w: repeatable([
 			"cancelSelection",
+			{
+				// move right when it's boundary of words, spaces, or newline (only zero or one char if it's newline)
+				command: "cursorRight",
+				when: "/(^.?$)|(\\s[^\\s])|([^\\s]\\s)|(.\\b.)/.test(_ctx.lineAt(_ctx.pos.line).text.substring(_ctx.pos.character, _ctx.pos.character+2))"
+			},
 			"cursorWordStartRightSelect",
+			// move left because the range is inclusive
+			"cursorLeftSelect"
 		]),
 		b: repeatable([
 			"cancelSelection",
@@ -193,7 +226,16 @@ module.exports = {
 		j: repeatable("cursorDownSelect"),
 		k: repeatable("cursorUpSelect"),
 		l: repeatable("cursorRightSelect"),
-		w: repeatable("cursorWordStartRightSelect"),
+		w: repeatable([
+			{
+				// move right when it's boundary of words, spaces, or newline (only zero or one char if it's newline)
+				command: "cursorRightSelect",
+				when: "/(^.?$)|(\\s[^\\s])|([^\\s]\\s)|(.\\b.)/.test(_ctx.lineAt(_ctx.pos.line).text.substring(_ctx.pos.character, _ctx.pos.character+2))"
+			},
+			"cursorWordStartRightSelect",
+			// move left because the range is inclusive
+			"cursorLeftSelect"
+		]),
 		b: repeatable("cursorWordStartLeftSelect"),
 		f: {
 			"": repeatable({
@@ -252,12 +294,20 @@ module.exports = {
 			},
 			e: "cursorBottomSelect",
 		},
+
+		// match mode
+		m: {
+			m: "editor.action.jumpToBracketSelect"
+		},
+		
+		// set back to normal mode
+		v: "modalEditor.setNormalMode"
 	},
 
 	// Command mode
 	command: {
 		// save file
-		"w": "workbench.action.files.save"
+		w: "workbench.action.files.save"
 	}
 };
 
